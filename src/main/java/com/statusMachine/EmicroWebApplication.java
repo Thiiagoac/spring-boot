b95bea8e1d0dd999d1c118a9c0217aff915ecea6
Thiago Araujo Cardoso <thiiagoac@hotmail.com>
2019-02-04 23:18:05 -0300

    Criação de projeto

---
+++ src/main/java/com/statusMachine/EmicroWebApplication.java
@@ -0,0 +1,15 @@
+package com.statusMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmicroWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmicroWebApplication.class, args);
+		System.out.println("pronto");
+	}
+
+}
+
