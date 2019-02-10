2283fb631af06be8c1c991ac2dce36ec7ff1eb7c
thiiagoac <thiiagoac@hotmail.com>
2019-02-10 12:42:58 -0300

    endpoint cliente/id

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -28,7 +29,8 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpf;
 	private Integer tipo;
-
+	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
