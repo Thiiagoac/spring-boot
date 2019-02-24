04d4b14b9ff71fe50870e1b4fa19240226b278a8
thiiagoac <thiiagoac@hotmail.com>
2019-02-24 19:53:22 -0300

    post cliente

--- src/main/java/com/webstore/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/webstore/cursomc/resources/CategoriaResource.java
@@ -38,7 +38,8 @@ public class CategoriaResource {
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
 		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
--- src/main/java/com/webstore/cursomc/services/ClienteService.java
+++ src/main/java/com/webstore/cursomc/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.webstore.cursomc.domain.Cidade;
 import com.webstore.cursomc.domain.Cliente;
+import com.webstore.cursomc.domain.Endereco;
+import com.webstore.cursomc.domain.enums.TipoCliente;
 import com.webstore.cursomc.dto.ClienteDTO;
+import com.webstore.cursomc.dto.ClienteNewDTO;
 import com.webstore.cursomc.repositories.ClienteRepository;
+import com.webstore.cursomc.repositories.EnderecoRepository;
 import com.webstore.cursomc.services.exception.DataIntegrityException;
 import com.webstore.cursomc.services.exception.ObjectNotFoundException;
 
@@ -21,6 +26,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository endrepo;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -30,7 +38,9 @@ public class ClienteService {
 
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		endrepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -65,4 +75,37 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), 
+				objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro()
+				, objDto.getNumero()
+				, objDto.getComplemento()
+				, objDto.getBairro()
+				, objDto.getCep()
+				, cli, new Cidade(objDto.getCidadeId(), null, null));
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
+	}
 }
+
+
+
+
+
+
+
+
+
+
