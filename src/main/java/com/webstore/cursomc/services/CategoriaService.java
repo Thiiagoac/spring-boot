8643ad239c69bc8342fb8e3a14f95b46c2a7636c
thiiagoac <thiiagoac@hotmail.com>
2019-02-24 13:45:56 -0300

    delete categorias

--- src/main/java/com/webstore/cursomc/services/CategoriaService.java
+++ src/main/java/com/webstore/cursomc/services/CategoriaService.java
@@ -13,34 +13,33 @@ import com.webstore.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
-	
+	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
+			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não foi possivel deletar a categoria ela possui produtos");
 		}
 	}
-	
 
 }
